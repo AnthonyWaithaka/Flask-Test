42e4934dc338c3347690deec0f513d809c021340
AnthonyWaithaka <athumbi1@gmail.com>
2017-07-18 18:33:54 +0300

    FIX: Removed unnecessary files

--- hello_world.py
+++
@@ -1,8 +0,0 @@
-# hello_world.py
-
-from flask import Flask
-app = Flask(__name__) #the _name_ argument indicates the app's module or package
-
-@app.route('/')#decorator, tells Flask which path to display the result of the function
-def hello_world():
-    return 'Hello World!'
