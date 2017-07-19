78b6af0f74bd6d77657577e973041f5aa344253e
AnthonyWaithaka <athumbi1@gmail.com>
2017-07-19 14:33:08 +0300

    FIX: valid tests and working code

--- app/server.py
+++ app/server.py
@@ -4,11 +4,10 @@ import re
 from app.client import bluClient
 
 client_email_list = []
-client_email_counter = 0
 client_password_list = []
-client_password_counter = 0
 client_id_list = []
 client_list = {}
+client_access_list = {}
 
 class bluServer(object):
     
@@ -27,7 +26,6 @@ class bluServer(object):
 
     def checkEmailRepeat(self, userEmail):
         global client_email_list
-        global client_email_counter
         
         checker = False
         for i in client_email_list:
@@ -46,10 +44,21 @@ class bluServer(object):
                 return checker
         return checker
 
-    def createClient(self, userEmail, userPassword, terms_and_conditions):
+    def checkUsernameRepeat(self, userName):
+        global client_id_list
+
+        checker = False
+        for i in client_id_list:
+            if i == userName:
+                checker = True
+                return checker
+        return checker
+
+    def createClient(self, userEmail, userName, userPassword, terms_and_conditions):
         global client_list
         global client_email_counter
         global client_email_list
+        global client_id_list
         global client_password_counter
         global client_password_list
 
@@ -67,20 +76,54 @@ class bluServer(object):
         if self.checkPasswordRepeat(userPassword) is True:
             return 4
 
-        client_email_counter += 1
-        client_email_list.insert((client_email_counter-1), userEmail)
+        if self.checkUsernameRepeat(userName) is True:
+            return 5
 
-        client_password_counter += 1
-        client_password_list.insert((client_password_counter-1), userPassword)
+        client_email_list.append(userEmail)
+
+        client_password_list.append(userPassword)
         
-        #use client_email_counter as client_id
-        client_id_list.append(client_email_counter)
+        client_id_list.append(userName)
 
-        #add to client_list dictionary with {client_id:(client_email, pwd)}
-        client_list.update({client_id_list[client_email_counter-1]:(client_email_list[client_email_counter-1],client_password_list[client_password_counter-1])})
+        client_list.update({userName:(userEmail,userPassword)})
 
         #create a client object initialized with client_id, email_address and pwd
-        newClient = bluClient(client_id_list[client_email_counter-1], userEmail, userPassword)
+        newClient = bluClient(userName, userEmail, userPassword)
+
+        #add client_id and associated object to client_access_list
+        client_access_list.update({userName:newClient})
 
         #return the new client object
         return newClient
+
+    def deleteClient(self, bluclient):
+        #check if respective client_id exists as a key in client_access_list
+        #if so, 
+        # find userEmail and userPassword from client_list
+        # delete userName, userEmail and userPassword from client_id_list client_email_list and client_password_list
+        # delete key-pair for client_access_list and client_list
+        #Tests: 
+        # 1. object of type bluClient not passed as argument
+        # 2. respective client_id does not exist in client_access_list
+        # 3. client_id and object pair still exist in client_access_list or client_list
+        # 4. client_id
+        pass
+
+    def viewClient(self, bluclient):
+        #check if bluclient exists as a key in client_access_list
+        #if so, 
+        # return bluClient's bucketlist object
+        #Tests:
+        # 1. object of type bluClient not passed as argument
+        # 2. client_id does not exist
+        # 3. client_id does not exist in client_access_list
+        # 4. function does not return bluBucketList object
+        pass
+    
+    def searchClient(self, userName):
+        #check if userName exists in client_id_list
+        #if so, return userName
+        #Tests:
+        # 1. userName does not exist in client_id_list
+        # 2. does not return userName
+        pass
--- test/test_server.py
+++ test/test_server.py
@@ -14,24 +14,30 @@ class ServerTestCase(unittest.TestCase):
         self.assertNotEqual(self.bServer.validateEmail('something@yes.com'), False, "Not a Valid Email Address")
 
     def test_create_client_tAndC_fail(self):
-        new_client = self.bServer.createClient('something@yes.com', 'aaa1111', True)
+        new_client = self.bServer.createClient('something@yes.com', 'thisguy', 'aaa1111', True)
         self.assertNotEqual(new_client, 1, "Must Accept Terms and Conditions")
 
     def test_create_client_email_valid(self):
-        new_client = self.bServer.createClient('something@yes.com', 'aaa1111', True)
+        new_client = self.bServer.createClient('something@yes.com', 'thisguy', 'aaa1111', True)
         self.assertNotEqual(new_client, 2, "Invalid Email Address")
 
     def test_create_client_email_repetition(self):
-        new_client = self.bServer.createClient('something@yes.com', 'aaa1111', True)
-        new_client2 = self.bServer.createClient('something@no.com', 'bbb1122', True)
+        new_client = self.bServer.createClient('something@yes.com', 'thisguy', 'aaa1111', True)
+        new_client2 = self.bServer.createClient('something@no.com', 'thatguy', 'bbb1112', True)
         self.assertNotEqual(new_client2, 3, "Client Email address already in use")
 
     def test_create_client_password_repetition(self):
-        new_client = self.bServer.createClient('something@yes.com', 'aaa1122', True)
-        new_client2 = self.bServer.createClient('something1@yes.com', 'aaa1123', True)
+        new_client = self.bServer.createClient('something1@yes.com', 'whichguy', 'aaa1113', True)
+        new_client2 = self.bServer.createClient('something1@no.com', 'anotherguy', 'aaa1114', True)
         self.assertNotEqual(new_client2, 4, "Client Password already in use")
 
+    def test_create_client_username_repetition(self):
+        new_client = self.bServer.createClient('something2@yes.com', 'thisone', 'aaa1115', True)
+        new_client2 = self.bServer.createClient('something2@no.com', 'thatone', 'aaa1116', True)
+        self.assertNotEqual(new_client2, 4, "Client Username already in use")
+
     def test_client_is_valid_object(self):
-        new_client = self.bServer.createClient('something@yes.com', 'aaa1122', True)
-        new_client2 = self.bServer.createClient('something1@yes.com', 'aaa1123', True)
+        new_client = self.bServer.createClient('something@yes.com', 'theseguys', 'aaa1117', True)
+        new_client2 = self.bServer.createClient('something3@yes.com', 'thoseguys', 'aaa1118', True)
         self.assertIsInstance(new_client2, bluClient, "Invalid Client Object")
+    #Observation, cannot use the same data pairs as previous test function. It will raise an error
